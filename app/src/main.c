dd50e38b21c399a10a27133e27a2579ee8b1b101
Eberhard Binder <bindere@tec-bindere-nb3.windows.reutlingen-university.de>
2026-09-02 16:13:13 +0200

    Complete assignment work

--- app/src/main.c
+++ app/src/main.c
@@ -5,27 +5,75 @@ LOG_MODULE_REGISTER(demo, LOG_LEVEL_DBG);
 
 #define STACK_SIZE 1024
 
-#define PRIO_A 5
-#define PRIO_B 5
+#define PRIO_LOW 	7
+#define PRIO_MED 	5
+#define PRIO_HIGH	3 
+#define PRIO_COOP	-1 
 
-void thread_a_fn(void *p1, void *p2, void *p3)
-{
-    while (1) {
-        k_msleep(200);
-    }
-}
 
-void thread_b_fn(void *p1, void *p2, void *p3)
+void t_low_fn(void *p1, void *p2, void *p3)
 {
+
+    int step = 0;
+
     while (1) {
+        LOG_INF("[LOW] step %d  tick=%u", step++, k_uptime_get_32());
         k_msleep(300);
     }
 }
 
-K_THREAD_DEFINE(thread_a, STACK_SIZE, thread_a_fn,
-                NULL, NULL, NULL, PRIO_A, 0, 0);
-K_THREAD_DEFINE(thread_b, STACK_SIZE, thread_b_fn,
-                NULL, NULL, NULL, PRIO_B, 0, 0);
+
+void t_med_fn(void *p1, void *p2, void *p3)
+{
+    int step = 0;
+
+    while (1) {
+        LOG_INF("[MED] step %d  tick=%u", step++, k_uptime_get_32());
+        k_msleep(200);
+    }
+}
+
+
+void t_high_fn(void *p1, void *p2, void *p3)
+{
+
+    int step = 0;
+
+    while (1) {
+        LOG_INF("[HIGH] step %d  tick=%u", step++, k_uptime_get_32());
+        k_msleep(100);
+    }
+}
+
+
+
+void t_coop_fn(void *p1, void *p2, void *p3)
+{
+
+    int step = 0;
+
+    while (1) {
+        for(int i = 0; i < 5; i++) 
+	{
+            LOG_INF("[COOP] step %d  tick=%u", step++, k_uptime_get_32());
+	}
+        k_yield();
+    }
+}
+
+
+K_THREAD_DEFINE(t_low, STACK_SIZE, t_low_fn,
+                NULL, NULL, NULL, PRIO_LOW, 0, 0);
+
+K_THREAD_DEFINE(t_med, STACK_SIZE, t_med_fn,
+                NULL, NULL, NULL, PRIO_MED, 0, 0);
+
+K_THREAD_DEFINE(t_high, STACK_SIZE, t_high_fn,
+                NULL, NULL, NULL, PRIO_HIGH, 0, 0);
+
+K_THREAD_DEFINE(t_coop, STACK_SIZE, t_coop_fn,
+                NULL, NULL, NULL, PRIO_COOP, 0, 0);
+
 
 int main(void)
 {
